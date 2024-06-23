bcaf9b1d6a9b6f109603bbee350a5277863cebd1
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2024-06-23 17:02:47 -0300

    Routers and more 20240623

--- Frontend/HardwareHaven/src/app/app.routes.ts
+++ Frontend/HardwareHaven/src/app/app.routes.ts
@@ -1,6 +1,17 @@
-import { Routes } from '@angular/router';
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component.js';
+import { ProductListComponent } from './product-list/product-list.component.js';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-];
+  { path: 'home', component: HomeComponent},
+  { path: 'productList', component: ProductListComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' } 
+  ];
+
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
