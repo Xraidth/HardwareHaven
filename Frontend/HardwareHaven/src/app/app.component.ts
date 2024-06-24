2a4a7128ceb1edd684d696ec62446286647f6166
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2024-06-23 23:41:44 -0300

    ProdructList 20240623

--- Frontend/HardwareHaven/src/app/app.component.ts
+++ Frontend/HardwareHaven/src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { HomeComponent } from './home/home.component.js';
-
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
-  template: '<home></home>',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent, CommonModule, ProductListComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-
+export class AppComponent implements OnInit {
   title = 'HardwareHaven';
+  page = '';
 
+  ngOnInit(): void {
+    this.setPage('home');
+  }
+
+  setPage(p: string) {
+    this.page = p;
+  }
 }
+
