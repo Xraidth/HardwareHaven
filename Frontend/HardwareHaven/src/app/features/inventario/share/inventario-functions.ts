bee4417bde95c603296807065b73fe6c614361e1
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2024-09-20 22:50:59 -0300

    changes 202409202250

--- Frontend/HardwareHaven/src/app/features/inventario/share/inventario-functions.ts
+++ Frontend/HardwareHaven/src/app/features/inventario/share/inventario-functions.ts
@@ -47,3 +47,12 @@ export function specialFiltro(nombre: string, dato: any): string {
   }
   
 
+export  function formatDateToYYYYMMDD(isoDate: string): string {
+    const date = new Date(isoDate);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`; 
+
+
+}
