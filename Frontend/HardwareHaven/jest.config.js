f82a623b83e88aa5a3d8038c7bf838b18a423b13
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2025-02-28 23:15:05 -0300

    changes 20250228 test

---
+++ Frontend/HardwareHaven/jest.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  displayName: 'mi-sitio-web',
+  maxWorkers: 3,
+  bail: true,
+  preset: 'jest-preset-angular',
+  roots: ['<rootDir>/src/'],
+ testMatch: ['**/+(*.)+(spec).+(ts)'],
+  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+  collectCoverage: true,
+  cacheDirectory: '<rootDir>/jestCache',
+  coverageReporters: ['text-summary', 'lcov'],
+  coverageDirectory: 'coverage/mi-sitio-web',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular'
+  },
+};
