bf0c7a6a5cc0963fa0340c93d428a21e6967f466
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2025-03-11 19:13:42 -0300

    aunthchanges 202503111913

--- Frontend/HardwareHaven/src/app/guards/auth.guard.ts
+++ Frontend/HardwareHaven/src/app/guards/auth.guard.ts
@@ -1,17 +1,34 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { SessionService } from '../core/services/share/session.service';
+import { ShareService } from '../core/services/share/share.service';
+import { firstValueFrom } from 'rxjs';
 
-export const authGuard: CanActivateFn = (route, state) => {
-
-  const isAuthenticated = SessionService.rememberSession() ? true : false;
+export const authGuard: CanActivateFn = async (route, state) => {
+  const shareService = inject(ShareService);
   const router = inject(Router);
 
-  if (!isAuthenticated) {
-       router.navigate(['/no-access']);
+  try {
+    const serverFlag = await checkServer(shareService);
+    const isAuthenticated = SessionService.rememberSession() && serverFlag;
+
+    if (!isAuthenticated) {
+      router.navigate(['/no-access']);
       return false;
     }
 
     return true;
+  } catch {
+    router.navigate(['/no-access']);
+    return false;
   }
+};
 
+async function checkServer(shareServer: ShareService): Promise<boolean> {
+  try {
+    await firstValueFrom(shareServer.ComeOn());
+    return true;
+  } catch {
+    return false;
+  }
+}
