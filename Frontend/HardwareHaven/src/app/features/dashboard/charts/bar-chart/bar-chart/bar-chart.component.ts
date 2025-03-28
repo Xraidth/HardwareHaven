96d55eeeee3e1b6bba9d8e64858ed97aee2220ce
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2025-03-28 19:04:09 -0300

    dashboard 202503281903

---
+++ Frontend/HardwareHaven/src/app/features/dashboard/charts/bar-chart/bar-chart/bar-chart.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { Color, colorSets, NgxChartsModule, ScaleType } from '@swimlane/ngx-charts';
+
+@Component({
+  selector: 'app-bar-chart',
+  standalone: true,
+  imports: [NgxChartsModule],
+  templateUrl: './bar-chart.component.html',
+  styleUrl: './bar-chart.component.css'
+})
+export class BarChartComponent {
+  view: [number, number] = [600, 300]; // Tamaño del gráfico
+
+  data = [
+    { name: 'Jan', value: 2000 },
+    { name: 'Feb', value: 2500 },
+    { name: 'Mar', value: 3000 },
+    { name: 'Apr', value: 2800 },
+    { name: 'May', value: 3200 },
+    { name: 'Jun', value: 3500 },
+    { name: 'Jul', value: 3700 },
+    { name: 'Aug', value: 3400 },
+    { name: 'Sep', value: 4000 },
+    { name: 'Oct', value: 4200 },
+    { name: 'Nov', value: 4500 },
+    { name: 'Dec', value: 4800 },
+  ];
+
+  // Opciones
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = false;
+  showXAxisLabel = true;
+  xAxisLabel = 'Meses';
+  showYAxisLabel = true;
+  yAxisLabel = 'Ingresos ($)';
+
+  // Esquema de color con mayor contraste en fondo blanco
+
+
+  colorScheme: Color = {
+    name: 'custom',            // Nombre del esquema de colores
+    selectable: true,          // Si el esquema es seleccionable
+    group: ScaleType.Ordinal,          // Tipo de grupo, puede ser 'Ordinal' o 'Linear'
+    domain: ['#808080', '#000000'], // Gris y negro
+  };
+
+}
+
--- Frontend/HardwareHaven/src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ Frontend/HardwareHaven/src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { DashboardCardComponent } from '../../../features/dashboard/dashboard-card/dashboard-card.component';
+import { BarChartComponent } from '../../../features/dashboard/charts/bar-chart/bar-chart/bar-chart.component';
 
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [DashboardCardComponent],
+  imports: [DashboardCardComponent, BarChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
