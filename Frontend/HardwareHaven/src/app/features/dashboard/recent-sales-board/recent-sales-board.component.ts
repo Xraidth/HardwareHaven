27c8c4f91e187f7dae46e8571f1a64ad98d73cc1
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2025-03-29 00:39:10 -0300

    20250329 0039

--- Frontend/HardwareHaven/src/app/features/dashboard/recent-sales-board/recent-sales-board.component.ts
+++ Frontend/HardwareHaven/src/app/features/dashboard/recent-sales-board/recent-sales-board.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecentSalesCardComponent } from './recent-sales-card/recent-sales-card.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recent-sales-board',
   standalone: true,
-  imports: [],
+  imports: [RecentSalesCardComponent, CommonModule],
   templateUrl: './recent-sales-board.component.html',
   styleUrl: './recent-sales-board.component.css'
 })
-export class RecentSalesBoardComponent {
+export class RecentSalesBoardComponent implements OnInit{
+  public users: any[] = [];
 
+  ngOnInit(): void {
+    this.users = [
+      { username: 'Username1', email: 'user1@gmail.com', recentSalesValue: 50000 },
+      { username: 'Username2', email: 'user2@gmail.com', recentSalesValue: 32000 },
+      { username: 'Username3', email: 'user3@gmail.com', recentSalesValue: 10000 }
+    ];
+  }
 }
--- Frontend/HardwareHaven/src/app/features/dashboard/recent-sales-board/recent-sales-card/recent-sales-card.component.ts
+++ Frontend/HardwareHaven/src/app/features/dashboard/recent-sales-board/recent-sales-card/recent-sales-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recent-sales-card',
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './recent-sales-card.component.html',
   styleUrl: './recent-sales-card.component.css'
 })
-export class RecentSalesCardComponent {
-
+export class RecentSalesCardComponent{
+@Input() user: any;
 }
