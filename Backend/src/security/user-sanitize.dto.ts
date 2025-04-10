f564a556a52bfa3cfc1b08877b2f07aec29ea503
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2025-04-09 21:02:57 -0300

    changes 20250409 2102 validations

--- Backend/src/security/user-sanitize.dto.ts
+++ Backend/src/security/user-sanitize.dto.ts
@@ -10,11 +10,18 @@ export function sanitizeUserInput(req: CustomRequest, res: Response, next: NextF
     req.body.sanitizedUser = {
         name: req.body.nameUser,
         password: req.body.password,
+        email:req.body.email,
+        tipoUsuario:req.body.tipoUsuario,
+        fechaNac:req.body.fechaNac,
+        sexo:req.body.sexo,
+        direccion:req.body.direccion,
         newPassword:req.body.newPassword,
         oldPassword:req.body.oldPassword,
         newUserName:req.body.newUserName,
-        tipoUsuario:req.body.tipoUsuario,
-        newUserType:req.body.newUserType
+        newUserType:req.body.newUserType,
+        newBirthDate:req.body.newBirthDate,
+        newSex:req.body.newSex,
+        newAddress:req.body.newAddress
       };
   
     Object.keys(req.body.sanitizedUser).forEach((key) => {
